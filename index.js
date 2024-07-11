ed695393d94ae9f786761d1789356cddb7aace14
Lucas Farias <lucasfarias.ln@gmail.com>
2024-07-11 10:44:00 -0300

    feat: add CreateUserController and validations

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 /* eslint-disable no-undef */
 import 'dotenv/config.js'
 import express from 'express'
+import { CreateUserController } from './src/controllers/create-user.js'
 
 const app = express()
 
 app.use(express.json())
+
+app.post('/api/users', async (request, response) => {
+  const createUserController = new CreateUserController()
+
+  const createUserResponse = await createUserController.execute(request)
+
+  response.status(createUserResponse.statusCode).send(createUserResponse.body)
+})
 
 app.listen(process.env.PORT, () =>
   console.log(`listening on port ${process.env.PORT}`),
---
+++ src/controllers/create-user.js
@@ -0,0 +1,49 @@
+import validator from 'validator'
+import { CreateUserUseCase } from '../use-cases/create-user.js'
+import { baadRequest, created, serverError } from './helpers.js'
+
+export class CreateUserController {
+  async execute(htppRequest) {
+    try {
+      const params = htppRequest.body
+      // validar a requisição (campos obrigatórios, tamanho de senha e e-mail)
+      const requiredFields = ['first_name', 'last_name', 'email', 'password']
+
+      for (const field of requiredFields) {
+        if (!params[field] || params[field].trim().length === 0) {
+          return baadRequest({ message: `Missing param: ${field}` })
+        }
+      }
+
+      const passwordIsValid = params.password.length < 6
+
+      if (passwordIsValid) {
+        return baadRequest({
+          message: 'Password must be at least 6 characters',
+        })
+      }
+
+      const emailIsValid = validator.isEmail(params.email)
+
+      if (!emailIsValid) {
+        return baadRequest({
+          message: 'Invalid e-mail, please provide valid one.',
+        })
+      }
+
+      // Chamar o use-case
+
+      const createUserUseCase = new CreateUserUseCase()
+
+      const createdUser = await createUserUseCase.execute(params)
+
+      // retornar o status code para o usuário
+
+      return created(createdUser)
+    } catch (error) {
+      console.error(error)
+
+      return serverError()
+    }
+  }
+}
