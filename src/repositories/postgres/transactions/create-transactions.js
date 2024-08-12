65f6cb593827c0c44a212652ec6e10f64adc0fe4
Lucas Farias <lucasfarias.ln@gmail.com>
2024-08-12 11:13:05 -0300

    feat: add createTransaction repository

---
+++ src/repositories/postgres/transactions/create-transactions.js
@@ -0,0 +1,24 @@
+import { PostgresClient } from '../../../db/postgres/client'
+
+export class PostgresCreateTransactionsRepository {
+  async execute(createTransactionParams) {
+    const createdTransactions = await PostgresClient.query(
+      ` 
+      INSERT INTO transactions 
+      (id, user_id, name, date, amount, type) 
+      VALUES ($1, $2, $3, $4, $5, $6) 
+      RETURNING * 
+      `,
+      [
+        createTransactionParams.id,
+        createTransactionParams.user_id,
+        createTransactionParams.name,
+        createTransactionParams.date,
+        createTransactionParams.amount,
+        createTransactionParams.type,
+      ],
+    )
+
+    return createdTransactions[0]
+  }
+}
