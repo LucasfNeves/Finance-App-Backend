92a3f3f0800c814d8028d0f0311e3f20e931150c
Lucas Farias <lucasfarias.ln@gmail.com>
2024-08-20 14:31:42 -0300

    feat: add to CreateTransacyionController

--- src/controllers/transactions/update-transaction.js
+++ src/controllers/transactions/update-transaction.js
@@ -1,9 +1,6 @@
-import validator from 'validator'
+import { ZodError } from 'zod'
+import { createtransactionSchema } from '../../schemas/transaction.js'
 import { baadRequest, ok, serverError } from '../helpers/index.js'
-import {
-  checkIfIdIsValid,
-  generateInvalidIdResponse,
-} from '../helpers/index.js'
 
 export class UpdateTransactionController {
   constructor(updateTransactionUseCase) {
@@ -12,59 +9,63 @@ export class UpdateTransactionController {
 
   async execute(httpRequest) {
     try {
-      const idIsValid = checkIfIdIsValid(httpRequest.params.transactionId)
-
-      if (!idIsValid) {
-        return generateInvalidIdResponse()
-      }
-
       const params = httpRequest.body
 
-      const allowedFields = ['name', 'date', 'amount', 'type']
+      await createtransactionSchema.parseAsync(params)
 
-      const someFieldIsNotAllowed = Object.keys(params).some(
-        (field) => !allowedFields.includes(field),
-      )
-
-      if (someFieldIsNotAllowed) {
-        return baadRequest({
-          message: 'Some field is not allowed',
-        })
-      }
-
-      if (params.amount) {
-        const amountisValid = validator.isCurrency(params.amount.toString(), {
-          digits_after_decimal: [2],
-          allow_negatives: false,
-          decimal_separator: '.',
-        })
-
-        if (!amountisValid) {
-          return baadRequest({
-            message: 'The amount must be a valid curreny',
-          })
-        }
-      }
-
-      if (params.type) {
-        const type = params.type.trim().toUpperCase()
-
-        const typeValid = ['EARNING', 'EXPENSE', 'INVESTMENT'].includes(type)
-
-        if (!typeValid) {
-          return baadRequest({
-            message: 'The type must be EARNING, EXPENSE or INVESTMENT',
-          })
-        }
-      }
-
-      const transactions = await this.updateTransactionUseCase.execute(
-        httpRequest.params.transactionId,
-        params,
-      )
+      const transactions = await this.updateTransactionUseCase.execute(params)
 
       return ok(transactions)
+
+      // const idIsValid = checkIfIdIsValid(httpRequest.params.transactionId)
+
+      // if (!idIsValid) {
+      //   return generateInvalidIdResponse()
+      // }
+
+      // const allowedFields = ['name', 'date', 'amount', 'type']
+
+      // const someFieldIsNotAllowed = Object.keys(params).some(
+      //   (field) => !allowedFields.includes(field),
+      // )
+
+      // if (someFieldIsNotAllowed) {
+      //   return baadRequest({
+      //     message: 'Some field is not allowed',
+      //   })
+      // }
+
+      // if (params.amount) {
+      //   const amountisValid = validator.isCurrency(params.amount.toString(), {
+      //     digits_after_decimal: [2],
+      //     allow_negatives: false,
+      //     decimal_separator: '.',
+      //   })
+
+      //   if (!amountisValid) {
+      //     return baadRequest({
+      //       message: 'The amount must be a valid curreny',
+      //     })
+      //   }
+      // }
+
+      // if (params.type) {
+      //   const type = params.type.trim().toUpperCase()
+
+      //   const typeValid = ['EARNING', 'EXPENSE', 'INVESTMENT'].includes(type)
+
+      //   if (!typeValid) {
+      //     return baadRequest({
+      //       message: 'The type must be EARNING, EXPENSE or INVESTMENT',
+      //     })
+      //   }
+      // }
     } catch (error) {
+      if (error instanceof ZodError) {
+        return baadRequest({
+          message: error.errors[0].message,
+        })
+      }
       console.error(error)
       return serverError()
     }
