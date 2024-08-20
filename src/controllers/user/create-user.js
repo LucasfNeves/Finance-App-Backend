bf5c049a07dec07e804f5f430804daf9b4d674c3
Lucas Farias <lucasfarias.ln@gmail.com>
2024-08-19 21:56:18 -0300

    feat: add zod to createUserController

--- src/controllers/user/create-user.js
+++ src/controllers/user/create-user.js
@@ -1,13 +1,6 @@
-import { EmailAlreadyInUseError } from '../../errors/user.js'
-import {
-  baadRequest,
-  created,
-  serverError,
-  checkIfEmailIsValid,
-  checkIfPasswordIsValid,
-  generateEmailAlreadyInUseResponse,
-  generateInvalidPasswordResponse,
-} from '../helpers/index.js'
+import { createUserSchema } from '../../schemas/index.js'
+import { baadRequest, created, serverError } from '../helpers/index.js'
+import { ZodError } from 'zod'
 
 export class CreateUserController {
   constructor(createUserUseCase) {
@@ -16,26 +9,8 @@ export class CreateUserController {
   async execute(htppRequest) {
     try {
       const params = htppRequest.body
-      // validar a requisição (campos obrigatórios, tamanho de senha e e-mail)
-      const requiredFields = ['first_name', 'last_name', 'email', 'password']
 
-      for (const field of requiredFields) {
-        if (!params[field] || params[field].trim().length === 0) {
-          return baadRequest({ message: `Missing param: ${field}` })
-        }
-      }
-
-      const passwordIsValid = checkIfPasswordIsValid(params.password)
-
-      if (!passwordIsValid) {
-        return generateInvalidPasswordResponse()
-      }
-
-      const emailIsValid = checkIfEmailIsValid(params.email)
-
-      if (!emailIsValid) {
-        return generateEmailAlreadyInUseResponse()
-      }
+      await createUserSchema.parseAsync(params)
 
       // Chamar o use-case
 
@@ -44,10 +19,37 @@ export class CreateUserController {
       // retornar o status code para o usuário
 
       return created(createdUser)
+
+      // // validar a requisição (campos obrigatórios, tamanho de senha e e-mail)
+      // const requiredFields = ['first_name', 'last_name', 'email', 'password']
+
+      // for (const field of requiredFields) {
+      //   if (!params[field] || params[field].trim().length === 0) {
+      //     return baadRequest({ message: `Missing param: ${field}` })
+      //   }
+      // }
+
+      // const passwordIsValid = checkIfPasswordIsValid(params.password)
+
+      // if (!passwordIsValid) {
+      //   return generateInvalidPasswordResponse()
+      // }
+
+      // const emailIsValid = checkIfEmailIsValid(params.email)
+
+      // if (!emailIsValid) {
+      //   return generateEmailAlreadyInUseResponse()
+      // }
     } catch (error) {
-      if (error instanceof EmailAlreadyInUseError) {
-        return baadRequest({ message: error.message })
+      if (error instanceof ZodError) {
+        return baadRequest({
+          message: error.errors[0].message,
+        })
       }
+
+      // if (error instanceof EmailAlreadyInUseError) {
+      //   return baadRequest({ message: error.message })
+      // }
 
       console.error(error)
 
