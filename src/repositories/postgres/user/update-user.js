4216afcec30605c0faddcb0b50407039431ef9f4
Lucas Farias <lucasfarias.ln@gmail.com>
2024-08-20 21:57:59 -0300

    feat: ensure update repository  use Prisma

--- src/repositories/postgres/user/update-user.js
+++ src/repositories/postgres/user/update-user.js
@@ -1,32 +1,41 @@
-import { PostgresClient } from '../../../db/postgres/client.js'
+import { prisma } from '../../../../prisma/prisma.js'
 
 export class PostgresUpdateUserRepository {
   async execute(userId, updateUserParams) {
-    // Arrays para armazenar os campos e valores de atualização
-    const updateFields = []
-    const updateValues = []
-
-    // Itera sobre as chaves do objeto updateUserParams para criar as partes da consulta
-    Object.keys(updateUserParams).forEach((key) => {
-      updateFields.push(`${key} = $${updateValues.length + 1}`)
-      updateValues.push(updateUserParams[key])
+    const user = await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: updateUserParams,
     })
 
-    // Adiciona o userId como último parâmetro
-    updateValues.push(userId)
+    return user
 
-    // Cria a consulta de atualização
-    const updateQuery = `
-      UPDATE users
-      SET ${updateFields.join(', ')}
-      WHERE id = $${updateValues.length}
-      RETURNING *
-    `
+    // // Arrays para armazenar os campos e valores de atualização
+    // const updateFields = []
+    // const updateValues = []
 
-    // Executa a consulta
-    const result = await PostgresClient.query(updateQuery, updateValues)
+    // // Itera sobre as chaves do objeto updateUserParams para criar as partes da consulta
+    // Object.keys(updateUserParams).forEach((key) => {
+    //   updateFields.push(`${key} = $${updateValues.length + 1}`)
+    //   updateValues.push(updateUserParams[key])
+    // })
 
-    // Retorna a primeira linha do resultado
-    return result[0]
+    // // Adiciona o userId como último parâmetro
+    // updateValues.push(userId)
+
+    // // Cria a consulta de atualização
+    // const updateQuery = `
+    //   UPDATE users
+    //   SET ${updateFields.join(', ')}
+    //   WHERE id = $${updateValues.length}
+    //   RETURNING *
+    // `
+
+    // // Executa a consulta
+    // const result = await PostgresClient.query(updateQuery, updateValues)
+
+    // // Retorna a primeira linha do resultado
+    // return result[0]
   }
 }
