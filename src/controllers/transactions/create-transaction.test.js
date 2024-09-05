3af52080f859547267bf386c94621b85e6be6ed7
Lucas Farias <lucasfarias.ln@gmail.com>
2024-09-04 21:38:58 -0300

    test: ensure CreateTransactionController returns 400 when missing user_id

--- src/controllers/transactions/create-transaction.test.js
+++ src/controllers/transactions/create-transaction.test.js
@@ -19,7 +19,7 @@ describe('Create Transaction Controller', () => {
     },
   }
 
-  const makesut = () => {
+  const makeSut = () => {
     const createTransactionUseCasecre = new CreateTransactionUseCaseStub()
     const sut = new CreateTransactionController(createTransactionUseCasecre)
 
@@ -28,12 +28,28 @@ describe('Create Transaction Controller', () => {
 
   it('should return 201 when crating transaction sucessfuly', async () => {
     // arrange
-    const { sut } = makesut()
+    const { sut } = makeSut()
 
     //act
     const response = await sut.execute(baseHttpRequest)
 
     //assert
     expect(response.statusCode).toBe(201)
+  })
+
+  it('should return 400 when missing user_id', async () => {
+    // arrange
+    const { sut } = makeSut()
+
+    // act
+    const response = await sut.execute({
+      body: {
+        ...baseHttpRequest.body,
+        user_id: undefined,
+      },
+    })
+
+    // assert
+    expect(response.statusCode).toBe(400)
   })
 })
