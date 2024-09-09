8c22f34653feb2b9f69588af4847af13065dcd4e
Lucas Farias <lucasfarias.ln@gmail.com>
2024-09-09 20:30:17 -0300

    test: ensure  UpdateTransactionController return 200 when updating a transaction sucessfully

---
+++ src/controllers/transactions/update-transaction.test.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+
+import { faker } from '@faker-js/faker'
+import { UpdateTransactionController } from './update-transaction'
+
+describe('Update Transaction Controller', () => {
+  class UpdateTransactionUseCaseStub {
+    async execute() {
+      return {
+        user_id: faker.string.uuid(),
+        id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        date: faker.date.anytime().toISOString(),
+        type: 'EXPENSE',
+        amount: Number(faker.finance.amount()),
+      }
+    }
+  }
+
+  const makeSut = () => {
+    const updateTransactionUseCase = new UpdateTransactionUseCaseStub()
+    const sut = new UpdateTransactionController(updateTransactionUseCase)
+
+    return { sut, updateTransactionUseCase }
+  }
+
+  const baseHttpRequest = {
+    params: { transactionId: faker.string.uuid() },
+    body: {
+      name: faker.commerce.productName(),
+      date: faker.date.anytime().toISOString(),
+      type: 'EXPENSE',
+      amount: Number(faker.finance.amount()),
+    },
+  }
+
+  it('should return 200 when updating a transaction sucessfully', async () => {
+    //arrange
+    const { sut } = makeSut()
+
+    //act
+    const response = await sut.execute(baseHttpRequest)
+
+    //assert
+    expect(response.statusCode).toBe(200)
+  })
+})
