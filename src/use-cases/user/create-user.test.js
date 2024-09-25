d5ae4534a8c29ff2820ba08806efd4ffc5cb345e
Lucas Farias <lucasfarias.ln@gmail.com>
2024-09-25 20:36:36 -0300

    test: ensure CreateUserUseCase throws if idGeneratorAdapter throws

--- src/use-cases/user/create-user.test.js
+++ src/use-cases/user/create-user.test.js
@@ -131,4 +131,18 @@ describe('Create User Use Case', () => {
     // assert
     expect(promise).rejects.toThrow()
   })
+
+  it('should throw if IdGeneratorAdapter throws', async () => {
+    // arrange
+    const { sut, idGeneratorAdapter } = makeSut()
+    jest.spyOn(idGeneratorAdapter, 'execute').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    // act
+    const promise = sut.execute(user)
+
+    // assert
+    expect(promise).rejects.toThrow()
+  })
 })
