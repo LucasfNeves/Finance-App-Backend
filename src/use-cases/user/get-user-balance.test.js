899b37ae64fe362f863851128481731bdda7fd8b
Lucas Farias <lucasfarias.ln@gmail.com>
2024-09-26 09:42:54 -0300

    test: ensure throw UsernotFoundError if GetUserByIdRepository returns null

---
+++ src/use-cases/user/get-user-balance.test.js
@@ -0,0 +1,74 @@
+import { faker } from '@faker-js/faker'
+import { GetUserBalanceUseCase } from './get-user-balance'
+import { UserNotFoundError } from '../../errors/user'
+
+/* eslint-disable no-undef */
+describe('GetUserBalanceUseCase', () => {
+  const user = {
+    first_name: faker.person.firstName(),
+    last_name: faker.person.lastName(),
+    email: faker.internet.email(),
+    password: faker.internet.password({
+      length: 7,
+    }),
+  }
+
+  const userBalance = {
+    earnings: faker.finance.amount(),
+    expenses: faker.finance.amount(),
+    investments: faker.finance.amount(),
+    balance: faker.finance.amount(),
+  }
+
+  class GetUserBalanceRepositoryStub {
+    async execute() {
+      return userBalance
+    }
+  }
+
+  class GetUserByIdRepositoryStub {
+    async execute() {
+      return user
+    }
+  }
+
+  const makeSut = () => {
+    const getUserBalanceRepository = new GetUserBalanceRepositoryStub()
+    const getUserByIdRepository = new GetUserByIdRepositoryStub()
+    const sut = new GetUserBalanceUseCase(
+      getUserBalanceRepository,
+      getUserByIdRepository,
+    )
+
+    return {
+      sut,
+      getUserBalanceRepository,
+      getUserByIdRepository,
+    }
+  }
+
+  test('should get user balance sucessfully', async () => {
+    //arrange
+    const { sut } = makeSut()
+
+    //act
+    const result = await sut.execute(faker.string.uuid())
+
+    //assert
+    expect(result).toEqual(userBalance)
+  })
+
+  test('should throw UsernotFoundError if GetUserByIdRepository returns null ', async () => {
+    //arrange
+    const { sut, getUserByIdRepository } = makeSut()
+
+    jest.spyOn(getUserByIdRepository, 'execute').mockResolvedValue(null)
+    const userId = faker.string.uuid()
+
+    //act
+    const promisse = sut.execute(userId)
+
+    //assert
+    await expect(promisse).rejects.toThrow(new UserNotFoundError(userId))
+  })
+})
