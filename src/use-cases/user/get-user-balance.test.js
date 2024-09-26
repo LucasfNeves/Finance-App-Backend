9d4fc9e9551695173f00b85e77b31f2f4d2082ec
Lucas Farias <lucasfarias.ln@gmail.com>
2024-09-26 10:05:21 -0300

    test: ensure throw if GetUserByIdRepository throws

--- src/use-cases/user/get-user-balance.test.js
+++ src/use-cases/user/get-user-balance.test.js
@@ -100,4 +100,16 @@ describe('GetUserBalanceUseCase', () => {
     //assert
     expect(getUserBalanceRepositorySpy).toHaveBeenCalledWith(userId)
   })
+
+  test('should throw if GetUserByIdRepository throws ', async () => {
+    //arrange
+    const { sut, getUserByIdRepository } = makeSut()
+    jest.spyOn(getUserByIdRepository, 'execute').mockRejectedValue(new Error())
+
+    //act
+    const promise = sut.execute(faker.string.uuid())
+
+    //assert
+    await expect(promise).rejects.toThrow()
+  })
 })
