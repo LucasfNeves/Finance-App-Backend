c7efe03e970b7618595a87586374d67b6ab12327
Lucas Farias <lucasfarias.ln@gmail.com>
2024-09-28 20:00:47 -0300

    test: ensure throw icall DeleteTransactionRepository with correct values

---
+++ src/use-cases/transactions/delete-transaction.test.js
@@ -0,0 +1,58 @@
+import { faker } from '@faker-js/faker'
+import { DeleteTransactionUseCase } from './delete-transaction'
+
+/* eslint-disable no-undef */
+describe('DeleteTransactionUseCase', () => {
+  const transaction = {
+    id: faker.string.uuid(),
+    user_id: faker.string.uuid(),
+    name: faker.commerce.productName(),
+    date: faker.date.anytime().toISOString(),
+    type: 'EXPENSE',
+    amount: Number(faker.finance.amount()),
+  }
+
+  class DeleteTransactionRepositoryStub {
+    async execute(transactionId) {
+      return {
+        ...transaction,
+        id: transactionId,
+      }
+    }
+  }
+
+  const makeSut = () => {
+    const deleteTransactionRepository = new DeleteTransactionRepositoryStub()
+
+    const sut = new DeleteTransactionUseCase(deleteTransactionRepository)
+
+    return {
+      sut,
+      deleteTransactionRepository,
+    }
+  }
+
+  test('should delete transaction sucessfully', async () => {
+    // arrange
+    const { sut } = makeSut()
+
+    // act
+    const response = await sut.execute(transaction.id)
+
+    // assert
+    expect(response).toEqual({ ...transaction, id: transaction.id })
+  })
+
+  test('should call DeleteTransactionRepository with correct values', async () => {
+    // arrange
+    const { sut, deleteTransactionRepository } = makeSut()
+
+    const executeSpy = jest.spyOn(deleteTransactionRepository, 'execute')
+
+    // act
+    await sut.execute(transaction.id)
+
+    // assert
+    expect(executeSpy).toHaveBeenCalledWith(transaction.id)
+  })
+})
