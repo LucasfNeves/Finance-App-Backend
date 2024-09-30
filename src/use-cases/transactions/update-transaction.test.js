5b71e960536035ec49f59d63f79867d86f19611c
Lucas Farias <lucasfarias.ln@gmail.com>
2024-09-30 13:50:05 -0300

    test: ensure should update transaction sucessfully

---
+++ src/use-cases/transactions/update-transaction.test.js
@@ -0,0 +1,46 @@
+import { faker } from '@faker-js/faker'
+import { UpdateTransactionUseCase } from './update-transaction'
+
+/* eslint-disable no-undef */
+describe('UpdateTransactionUseCase', () => {
+  const createTransactionParams = {
+    id: faker.string.uuid(),
+    user_id: faker.string.uuid(),
+    name: faker.commerce.productName(),
+    date: faker.date.anytime().toISOString(),
+    type: 'EXPENSE',
+    amount: Number(faker.finance.amount()),
+  }
+
+  class UpdateTransactionRepositoryStub {
+    async execute(transactionId) {
+      return {
+        id: transactionId,
+        ...createTransactionParams,
+      }
+    }
+  }
+
+  const makeSut = () => {
+    const updateTransactionRepository = new UpdateTransactionRepositoryStub()
+    const sut = new UpdateTransactionUseCase(updateTransactionRepository)
+
+    return {
+      sut,
+      updateTransactionRepository,
+    }
+  }
+
+  test('should update transaction sucessfully', async () => {
+    // arrange
+    const { sut } = makeSut()
+
+    // act
+    const response = await sut.execute(createTransactionParams.id, {
+      amount: Number(faker.finance.amount()),
+    })
+
+    // assert
+    expect(response).toEqual(createTransactionParams)
+  })
+})
