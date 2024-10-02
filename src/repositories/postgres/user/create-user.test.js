42f5920950a1d011891974807a8dd1293d0bbf68
Lucas Farias <lucasfarias.ln@gmail.com>
2024-10-02 10:31:42 -0300

    test: ensure PostgresCreateUserRepository call Prisma with correct params

--- src/repositories/postgres/user/create-user.test.js
+++ src/repositories/postgres/user/create-user.test.js
@@ -1,3 +1,4 @@
+import { prisma } from '../../../../prisma/prisma'
 import { user } from '../../../tests/fixtures/user'
 import { PostgresCreateUserRepository } from './create-user'
 
@@ -13,5 +14,16 @@ describe('CreateUserRepository', () => {
     expect(result.last_name).toBe(user.last_name)
     expect(result.email).toBe(user.email)
     expect(result.password).toBe(user.password)
+  })
+
+  it('should PostgresCreateUserRepository call Prisma with correct params', async () => {
+    const sut = new PostgresCreateUserRepository()
+    const prismaSpy = jest.spyOn(prisma.user, 'create')
+
+    await sut.execute(user)
+
+    expect(prismaSpy).toHaveBeenCalledWith({
+      data: user,
+    })
   })
 })
